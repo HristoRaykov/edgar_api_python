1a5e0db16ee95ae407cb0875fea1bfb68ab0f3b1
Hristo Raykov <hristocr@gmail.com>
2020-09-25 21:31:37 +0300

    tiingo api tested

--- pdr.py
+++ pdr.py
@@ -5,13 +5,22 @@ import pandas_datareader as pdr
 query = 'WIKI/AAPL'
 data = pdr.DataReader('^DJI', 'stooq')
 
+# # Meta Data
+# https://api.tiingo.com/tiingo/daily/<ticker>
+#
+# # Latest Price
+# https://api.tiingo.com/tiingo/daily/<ticker>/prices
+#
+# # Historical Prices
+# https://api.tiingo.com/tiingo/daily/<ticker>/prices?startDate=2012-1-1&endDate=2016-1-1
 
-TIIANGO_API_KEY = os.getenv("TIIANGO_API_KEY")
+
+TIINGO_API_KEY = os.getenv("TIINGO_API_KEY")
 # https://api.tiingo.com/tiingo/daily/ETY/prices?token=5bbb9e7c4c5aab5c84531c9b8bc04c3b56848e21
-data = pdr.get_data_tiingo("XETYX", api_key=TIIANGO_API_KEY)
-tickers = pd.read_csv("supported_tickers_tiingo.csv")
-asset_types = tickers["assetType"].unique()
-exchange = tickers["exchange"].unique()
+data = pdr.get_data_tiingo("XETYX", api_key=TIINGO_API_KEY)
+
+tiingo_tickers = pd.read_csv("https://apimedia.tiingo.com/docs/tiingo/daily/supported_tickers.zip")
+
 
 
 data = pdr.get_data_yahoo("ETY", get_actions=True)  # adjust_price=True
